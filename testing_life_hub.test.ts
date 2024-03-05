fa5b615f5e1e6abaf80bdce8216a039dcbb71858
FelixFagerstedt <145350711+FelixFagerstedt@users.noreply.github.com>
2024-03-05 17:33:41 +0100

    Add files via upload

--- testing_life_hub.test.ts
+++ testing_life_hub.test.ts
@@ -1,7 +1,14 @@
  
 
 import { ph_empty, ph_insert, probe_linear, ph_lookup, ph_delete } from "../ Lib/hashtables";
-//import { saveCredentials, handleActionKeyPress2 } from "./LifeHub_albin";
+ 
+
+//Because of our problems with exporting functions from the mainfile, we have chosen to copy paste all functions
+//needed for the tests
+
+//Jest works in Node.js which have a problem with taking HTML Documents. Because of this we have chosen to change the the HTML 
+//inputs to Parameters. Because of the problem with HTML, the local storage is not accessible either and we solved that by creating a
+// local mock storage for hashtables and mock credentials.
 
 
  
@@ -20,11 +27,11 @@ function string_to_number(str: string): number {
     return sum;
 }
 
-type ProbingHashtable<K, V> = {                     // The type for a probing hashtable
-    readonly keys: Array<K | null | undefined>,     // readonly has been removed from probe  
-    readonly data: Array<V>,                        // because when the probing hashtable is 
-    probe: ProbingFunction<K>,                      // saved down with JSON stringify it 
-    size: number                                    // becomes undefined so it has to set 
+type ProbingHashtable<K, V> = {                      
+    readonly keys: Array<K | null | undefined>,       
+    readonly data: Array<V>,                         
+    probe: ProbingFunction<K>,                         
+    size: number                                     
 };        
 
 type ActivityTable = ProbingHashtable<string, Activity>     // Type for an activity table
@@ -32,7 +39,7 @@ type ActivityTable = ProbingHashtable<string, Activity>     // Type for an activ
 
 let credentials: { [key: string]: string } = {}
 
-type Activity = {               // The type for an acticity
+type Activity = {               
     Month: string,
     Date: string
     Start: string,
@@ -48,134 +55,103 @@ let Global_username: string = "Albin";
 
   
 
-     function saveCredentials_changed(username: string, password: string): void 
+function saveCredentials_changed(username: string, password: string): void 
     {
-        //const credentials: { [key: string]: string } = localStorage.getItem('credentials') ? JSON.parse(localStorage.getItem('credentials') as string) : {};
-        //credentials[username] = password;
-        //localStorage.setItem('credentials', JSON.stringify(credentials));
-        credentials[username] = password;
+    //const credentials: { [key: string]: string } = localStorage.getItem('credentials') ? JSON.parse(localStorage.getItem('credentials') as string) : {};
+    //credentials[username] = password;
+    //localStorage.setItem('credentials', JSON.stringify(credentials));
+    credentials[username] = password;
         
     }
    
-    function makeData_changed(month: string, date: string, start: string, end: string, activity: string): void {
+function makeData_changed(month: string, date: string, start: string, end: string, activity: string): void {
          
-        //const data_con: string = localStorage.getItem(Global_username + '_data') as string;
-        //const data: ActivityTable = data_con ? JSON.parse(data_con) : ph_empty(10, probe_linear(hashfunc));
-        const data: ActivityTable = mock_storage[Global_username + '_data'] ? mock_storage[Global_username + '_data'] : ph_empty(10, probe_linear(hashfunc));// for testing sake we just create a empty hashtable 
-        data.probe = probe_linear(hashfunc);
-        const aktivitet: Activity = {
-            Month: month,
-            Date: date,
-            Start: start,
-            End: end,
-            Activity: activity
-        };
-        const id: string = month + date + activity;
+    //const data_con: string = localStorage.getItem(Global_username + '_data') as string;
+    //const data: ActivityTable = data_con ? JSON.parse(data_con) : ph_empty(10, probe_linear(hashfunc));
+    const data: ActivityTable = mock_storage[Global_username + '_data'] ? mock_storage[Global_username + '_data'] : ph_empty(10, probe_linear(hashfunc));// for testing sake we just create a empty hashtable 
+    data.probe = probe_linear(hashfunc);
+    const aktivitet: Activity = {
+        Month: month,
+        Date: date,
+        Start: start,
+        End: end,
+        Activity: activity
+    };
+    const id: string = month + date + activity;
          
-        ph_insert(data, id, aktivitet);
-        //localStorage.setItem(Global_username + '_data', JSON.stringify(data)); // cant add the data to local storage
-        mock_storage[Global_username + '_data'] = data; // saving the data in storage created in file
-    }
+    ph_insert(data, id, aktivitet);
+    //localStorage.setItem(Global_username + '_data', JSON.stringify(data)); // cant add the data to local storage
+    mock_storage[Global_username + '_data'] = data; // saving the data in storage created in file
+}
     
      
-    function add_changed(month_value: string, date_value: string, start_value: string, end_value: string, activity_value: string ): void 
-     {
-        //added parameters instead of HTMLINputs because we are in node.js which cant read document files
+function add_changed(month_value: string, date_value: string, start_value: string, end_value: string, activity_value: string ): void {
+    //added parameters instead of HTMLINputs because we are in node.js which cant read document files
 
-        //const month: HTMLInputElement | null = document.getElementById("Month_a") as HTMLInputElement;
-        //const date: HTMLInputElement | null = document.getElementById("Date_a") as HTMLInputElement;
-        //const start: HTMLInputElement | null = document.getElementById("Start_a") as HTMLInputElement;
-        //const end: HTMLInputElement | null = document.getElementById("End_a") as HTMLInputElement;
-        //const activity: HTMLInputElement | null = document.getElementById("Activity_a") as HTMLInputElement;
+    //const month: HTMLInputElement | null = document.getElementById("Month_a") as HTMLInputElement;
+    //const date: HTMLInputElement | null = document.getElementById("Date_a") as HTMLInputElement;
+    //const start: HTMLInputElement | null = document.getElementById("Start_a") as HTMLInputElement;
+    //const end: HTMLInputElement | null = document.getElementById("End_a") as HTMLInputElement;
+    //const activity: HTMLInputElement | null = document.getElementById("Activity_a") as HTMLInputElement;
     
-        //if (month && date && start && end && activity) {
-            //const month_value: string = month.value;
-            //const date_value: string = date.value;
-            //const start_value: string = start.value;
-            //const end_value: string = end.value;
-            //const activity_value: string = activity.value;
-            makeData_changed(month_value, date_value, start_value, end_value, activity_value);
-        }
+    //if (month && date && start && end && activity) {
+        //const month_value: string = month.value;
+        //const date_value: string = date.value;
+        //const start_value: string = start.value;
+        //const end_value: string = end.value;
+        //const activity_value: string = activity.value;
+    makeData_changed(month_value, date_value, start_value, end_value, activity_value);
+}
 
-        function remove_changed(month_value: string, date_value: string, activity_value: string): void {
+function remove_changed(month_value: string, date_value: string, activity_value: string): void {
 
-            // adding parameters instead of HTMLinputs because Node.js cant take document files
+    // adding parameters instead of HTMLinputs because Node.js cant take document files
 
-            //const month: HTMLInputElement | null = document.getElementById("Month_r") as HTMLInputElement;
-            //const date: HTMLInputElement | null = document.getElementById("Date_r") as HTMLInputElement;
-            //const activity: HTMLInputElement | null = document.getElementById("Activity_r") as HTMLInputElement;
+    //const month: HTMLInputElement | null = document.getElementById("Month_r") as HTMLInputElement;
+    //const date: HTMLInputElement | null = document.getElementById("Date_r") as HTMLInputElement;
+    //const activity: HTMLInputElement | null = document.getElementById("Activity_r") as HTMLInputElement;
         
-            //if (month && date && activity) {
-                //const month_value: string = month.value;
-                //const date_value: string = date.value; 
-                //const activity_value: string = activity.value;
+    //if (month && date && activity) {
+        //const month_value: string = month.value;
+        //const date_value: string = date.value; 
+        //const activity_value: string = activity.value;
         
-                const id: string = month_value + date_value + activity_value
+    const id: string = month_value + date_value + activity_value
                  
 
-                //const data_con: string = localStorage.getItem(Global_username + '_data') as string;
-                //const data: ActivityTable = data_con ? JSON.parse(data_con) : ph_empty(10, probe_linear(hashfunc));
+    //const data_con: string = localStorage.getItem(Global_username + '_data') as string;
+    //const data: ActivityTable = data_con ? JSON.parse(data_con) : ph_empty(10, probe_linear(hashfunc));
                 
-                // because we dont need to convert the hashtable from the local storage on the browser we can just take the file 
-                // from our local storage
-                const data: ActivityTable = mock_storage[Global_username + '_data'] ? mock_storage[Global_username + '_data'] : ph_empty(10, probe_linear(hashfunc)); // added
-                data.probe = probe_linear(hashfunc);
+    // because we dont need to convert the hashtable from the local storage on the browser we can just take the file 
+    // from our local storage
+    const data: ActivityTable = mock_storage[Global_username + '_data'] ? mock_storage[Global_username + '_data'] : ph_empty(10, probe_linear(hashfunc)); // added
+    data.probe = probe_linear(hashfunc);
                  
-                if (ph_lookup(data, id)) {
-                    //showMessage("Aktiviteten har blivit bortagen")
-                    ph_delete(data, id);
-                    //localStorage.setItem(Global_username + '_data', JSON.stringify(data));
-                }
+    if (ph_lookup(data, id)) {
+        //showMessage("Aktiviteten har blivit bortagen")
+        ph_delete(data, id);
+        //localStorage.setItem(Global_username + '_data', JSON.stringify(data));
+    }
+}
+             
+function search_date_helper_changed(month: string, date: string): Activity[] {
+                     
+    //let data: ActivityTable = localStorage.getItem(Global_username + '_data') ? JSON.parse(localStorage.getItem(Global_username + '_data') as string) : ph_empty(10, probe_linear(hashfunc));
+    const data: ActivityTable = mock_storage[Global_username + '_data'] ? mock_storage[Global_username + '_data'] : ph_empty(10, probe_linear(hashfunc));
+    data.probe = probe_linear(hashfunc);
+    const activities: Activity[] = [];
+    for (let i = 0; i < data.keys.length; i++) {
+        const key = data.keys[i];
+        if (key !== null && key !== undefined) {
+            const storedActivity = ph_lookup(data, key);
+            if (storedActivity && storedActivity.Month === month && storedActivity.Date === date) {
+                activities.push(storedActivity);
             }
-            /*
-            function search_changed(searchMonth: string, searchDate: string): void {
-    
-                //const searchMonthInput: HTMLInputElement | null = document.getElementById("search_month") as HTMLInputElement;
-                //const searchDateInput: HTMLInputElement | null = document.getElementById("search_date") as HTMLInputElement;
-                //const messageElement: HTMLElement | null = document.getElementById("message");
-            
-                //if (searchMonthInput && searchDateInput && messageElement) {
-                   // const searchMonth: string = searchMonthInput.value;
-                   // const searchDate: string = searchDateInput.value;
-                    const activities: Activity[] = searchActivity(searchMonth, searchDate);
-            
-                    if (activities.length > 0) {
-                        let message = "Activities found for " + searchMonth + " " + searchDate + ":\n";
-                        activities.forEach(activity => {
-                            message += "Start: " + activity.Start + ", End: " + activity.End + ", Activity: " + activity.Activity + "\n";
-                        });
-                        messageElement.innerText = message;
-                    } else {
-                        messageElement.innerText = "No activities found for " + searchMonth + " " + searchDate;
-                    }
+        }
+    }
                     
-                }
-                */
-                function search_date_helper_changed(month: string, date: string): Activity[] {
-                     
-                     
-                    //let data: ActivityTable = localStorage.getItem(Global_username + '_data') ? JSON.parse(localStorage.getItem(Global_username + '_data') as string) : ph_empty(10, probe_linear(hashfunc));
-                    const data: ActivityTable = mock_storage[Global_username + '_data'] ? mock_storage[Global_username + '_data'] : ph_empty(10, probe_linear(hashfunc));
-                    data.probe = probe_linear(hashfunc);
-                    const activities: Activity[] = [];
-                    
-                      
-                
-                    for (let i = 0; i < data.keys.length; i++) {
-                        const key = data.keys[i];
-                        
-                        if (key !== null && key !== undefined) {
-                            const storedActivity = ph_lookup(data, key);
-                             
-                            
-                            if (storedActivity && storedActivity.Month === month && storedActivity.Date === date) {
-                                activities.push(storedActivity);
-                            }
-                        }
-                    }
-                    
-                    return activities; // Return array of activities for the specified date
-                }
+    return activities; // Return array of activities for the specified date
+}
             
  
 
@@ -187,7 +163,7 @@ test("create a user", () => {
     
 });
 
-test("make some data", () => {
+test("make data", () => {
     
     makeData_changed("january", "23", "13:00", "15:00", "Fotboll");
 
@@ -197,6 +173,7 @@ test("make some data", () => {
 });
 
 test("add a activity", () => {
+
     add_changed("january", "23", "13:00", "14:00", "Football");
 
     const calendar_data: ActivityTable = mock_storage[Global_username + '_data']; // accesing the correct users data
